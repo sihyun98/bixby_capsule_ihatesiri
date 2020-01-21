74ba5fefb2db51b60f55b13bc973dbd72431f9cd
Yangsu0 <yjy0186@naver.com>
2020-01-21 15:05:45 +0900

    almost finished

--- code/FindFood.js
+++ code/FindFood.js
@@ -1,11 +1,12 @@
-var http = require('http')
+//var http = require('http')
 var console = require('console')
 var config = require('config')
+var db = require('./data/db.js')
 module.exports.function = function findFood (item1,item2,item3,item4,item5) {
   // Read the remote.url value from capsule.properties
   var items = []
   var result = {}
-  var temp = new Array(0,0,0,0,0,0,0,0,0)
+  var temp = new Array(0,0,0,0)
 
   if (item1) {
       items.push(item1)
@@ -22,25 +23,34 @@ module.exports.function = function findFood (item1,item2,item3,item4,item5) {
   if (item5) {
     items.push(item5)
   }
-  
+  // var response = http.getUrl(config.get('remote.url'), { format: 'json' });
   for (var i in items){
-    var query = String(items[i])
-    var response = http.getUrl(config.get('remote.url')+'/DESC_KOR='+query, { format: 'json' });
-    response = response.I0750.row;
-        
-    for (var j = 0; j<9; j++){
-      var tp = String('NUTR_CONT'+Number(j+1))
-      if (Number(response[0][tp])>=0){
-        temp[j] = temp[j]+Number(response[0][tp])
-      }
-      else{
-        temp[j] = temp[j]+ (Number(response[0][tp]) || 0)
+    var flag = false
+    for (var cnt in db){
+      var target = db[cnt]['DESC_KOR']
+      if(target.indexOf(String(items[i]))!=-1 || String(items[i]).indexOf(target)!=-1){
+        for (var j = 0; j<4; j++){
+          var tp = String('NUTR_CONT'+Number(j+1))
+          if (Number(db[cnt][tp])>=0){
+            temp[j] = temp[j] + Number(db[cnt][tp])
+          }
+          else{
+            temp[j] = temp[j] + (Number(db[cnt][tp]) || 0)
+          }
+        }
+        flag = true
+        break
       }
     }
+    if(flag == false){
+      result[String('item'+(Number(i)+1))] = String(items[i])
+    }
   }
-  for (var k = 0; k<9; k++){
+  var list = new Array(2350, 370, 52.5, 52.075)
+  for (var k = 0; k<4; k++){
     var tp = String('NUTR_CONT'+Number(k+1))
-    result[tp] = temp[k]
+    result[String(tp+'_PER')] = Math.round((temp[k]/list[k])*100)
+    result[tp] = Math.round(temp[k])
   }
   return result;
 }
